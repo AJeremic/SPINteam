80b9efd1ae0726385af733170a35d1ca04892b47
AleksJere <130003125+AleksJere@users.noreply.github.com>
2023-06-30 01:03:50 -0800

    updated to vectors

--- SPIN code/spincode_attempt_1.py
+++ SPIN code/spincode_attempt_1.py
@@ -17,10 +17,12 @@ if __name__ == "__main__":
     
     
     K_u = 5       #uniaxial anisotropy coefficient
-    H = 5   #applied field, we will be either using this as a constant or sweeping it
+    r = 5 #magnitude of the applied field
+    H = np.array([r*np.cos(theta),r*np.sin(theta)])   #applied field, we will be either using this as a constant or sweeping it
     phi = 0  #angle between the applied field and the uniaxial anisotropy
     theta = 0 #angle between magnetisation and the applied field?
-    M = 1 # magentisation of the sample
+    M = 1 # magintude of the magentisation of the sample
+    Magn = np.array([M*np.cos(phi),M*np.sin(phi)])
     """
     naming of equations / terms
 
