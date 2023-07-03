1e168d8b7c2176a5ed8ae77d59872f09cd7be2de
AleksJere <130003125+AleksJere@users.noreply.github.com>
2023-07-03 10:57:21 +0200

    trying to change it to radians
    need help diego

--- SPIN code/spincode_attempt_1.py
+++ SPIN code/spincode_attempt_1.py
@@ -17,17 +17,19 @@ if __name__ == "__main__":
     
     """
     i = 0
-    theta = [0]
+    theta = [0.]
     for j in range(72): 
         i +=5
         theta.append(i) #angle between magnetisation and the applied field?
+        
+    theta *= np.pi/180
     K_u = 1100000       #uniaxial anisotropy coefficient
     r = 5 #magnitude of the applied field
-    #print(theta)
+    print(theta)
     phi = 0  #angle between the applied field and the uniaxial anisotropy
   
     M = 1 # magintude of the magentisation of the sample
-    Magn = np.array([M*np.cos(phi),M*np.sin(phi)]) # magnetisation as a vector
+    Magn = np.array([M*np.cos(phi*np.pi/180),M*np.sin(phi*np.pi/180)]) # magnetisation as a vector
     H = np.array([r*np.cos(theta),r*np.sin(theta)])   #applied field, we will be either using this as a constant or sweeping it
     # possible theta/phi changing scenario  theta = np.linspace(0:360:5)
     E_total = []
@@ -45,15 +47,15 @@ if __name__ == "__main__":
             desc = 0
     
         E_1 = K_u*(np.sin(theta[i]))**2
-        E_2 = r*M*np.cos(phi)
+        E_2 = r*M*np.cos(phi*np.pi/180)
         
         E_total.append(int(E_1 + E_2))
-        #print(theta[i])
+        print(theta[i])
         if desc:
             phi-=5
         else:
             phi+=5
-    plt.plot( theta,E_total)
+    plt.plot(theta,E_total)
     plt.title("energy")
     plt.show()
     
