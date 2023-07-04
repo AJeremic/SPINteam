5b0f2910557c24dba99cc60bef0d97d72d251e69
AleksJere <130003125+AleksJere@users.noreply.github.com>
2023-07-04 14:16:15 +0200

    Update spincode_attempt_1.py

--- SPIN code/spincode_attempt_1.py
+++ SPIN code/spincode_attempt_1.py
@@ -8,7 +8,24 @@ Created on Fri Jun 30 00:38:00 2023
 from collections import deque
 from matplotlib import pyplot as plt
 import numpy as np
+import scipy.signal as sig
 
+def peaksandtroughs(array):
+    peaks, _ = sig.find_peaks(E_total)
+    countmax = 0
+    countmin = 0
+    locationmin = np.array([])
+    locationmax = np.array([])
+    for peak in peaks:
+        countmax += 1
+        locationmax = np.append(locationmax,peak)
+    troughs = sig.argrelmin(E_total)
+    for relmin in troughs:
+        countmin += 1
+        locationmin = np.append(locationmin, relmin)
+    locationmax *= 5
+    locationmin *= 5
+    return locationmax, locationmin, countmax, countmin
 
 if __name__ == "__main__":
     
@@ -17,6 +34,10 @@ if __name__ == "__main__":
     naming of variables
     
     """
+    locationmax = np.array([])
+    locationmin = np.array([])
+    countmax = 0
+    countmin = 0
     i = x
     splice_const = int(i/5)+1
     theta = [0.]
@@ -26,15 +47,17 @@ if __name__ == "__main__":
     theta_t = np.array(theta)
     theta_t *= np.pi/180
     theta_r = np.mod(theta_t, 2*np.pi)
+    
 
     graphing_t = deque(theta_r)
 
     graphing_t.popleft()
 
     graphing_t.rotate(splice_const)
+    graphing_t.append(2*np.pi)
     
     K_u = 110000       #uniaxial anisotropy coefficient
-    r = 0 #magnitude of the applied field
+    r = 100000 #magnitude of the applied field
     #print(theta_r)
     phi = x%90  #angle between the applied field and the uniaxial anisotropy
   
@@ -42,7 +65,7 @@ if __name__ == "__main__":
     Magn = np.array([M*np.cos(phi*np.pi/180),M*np.sin(phi*np.pi/180)]) # magnetisation as a vector
     H = np.array([r*np.cos(theta_r),r*np.sin(theta_r)])   #applied field, we will be either using this as a constant or sweeping it
     # possible theta/phi changing scenario  theta = np.linspace(0:360:5)
-    E_total = []
+    E_total = np.array([])
     desc = 0
     #print(theta_t)
     #print("                                                         ")
@@ -63,7 +86,7 @@ if __name__ == "__main__":
         E_2 = 1/8*K_1*np.sin(2*phi*np.pi/180)**2
     
     """
-    for i in range(len(theta_r)-1):
+    for i in range(len(theta_r)):
         #print(i)
         if ((theta_r[i])%(np.pi/2) == 0 and (theta_r[i]) != np.pi and (theta_r[i]) != 0):
             desc = 1
@@ -85,8 +108,8 @@ if __name__ == "__main__":
         E_2 = r*M*np.cos(theta_r[i])
         #print(phi)
         
-        E_total.append(int(E_1 + E_2))
-        
+        E_total = np.append(E_total,int(E_1+E_2))
+        #print(E_total)
         #print(theta_r[i])
         if desc:
             phi-=5
@@ -98,14 +121,20 @@ if __name__ == "__main__":
     plotting
     
     """
+
+    locationmax, locationmin, countmax, countmin = peaksandtroughs(E_total)
     
-    a = np.amin(E_total)
-    #print(a)
+    print(locationmax)
+    print(locationmin)
+    
+
     plt.plot(graphing_t,E_total)
     plt.title("energy")
     plt.xlabel("Radians")
     plt.ylabel("energy in J/m^3")
     plt.show()
     
-    
+
+
+
     
